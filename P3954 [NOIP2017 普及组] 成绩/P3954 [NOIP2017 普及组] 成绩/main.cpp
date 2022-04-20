63c8e4bff2200b56c274fcd321e11a1515b1aaf7
stoneoceam <stoneoceam@163.com>
2022-04-20 12:34:25 +0800

    2022.4.20

---
+++ P3954 [NOIP2017 普及组] 成绩/P3954 [NOIP2017 普及组] 成绩/main.cpp
@@ -0,0 +1,13 @@
+//‘≠Ã‚¡¥Ω” https://www.luogu.com.cn/problem/P3954
+
+#include<iostream>
+using namespace std;
+int main() {
+	unsigned int A,B,C;
+	cin >> A >> B >> C;
+	
+	double sum;
+	sum = A * 0.2 + B * 0.3 + C * 0.5;
+	cout << sum;
+	return 0;
+}
